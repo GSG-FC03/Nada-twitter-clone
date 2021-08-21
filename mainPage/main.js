5107266c58967f5c64eb3b131cffc5df1e0480fe
NadaSaleh20 <nadasaleh987@gmail.com>
2021-08-22 00:48:31 +0300

    add function to display sidebar whwn click on chevron-down Relates #11

--- mainPage/main.js
+++ mainPage/main.js
@@ -3,3 +3,18 @@ window.onload = function() {
     document.getElementById('profilecardname').innerText =localStorage.getItem('name');
     document.getElementById('username').innerText =localStorage.getItem('name');
 };
+//display slider 
+const user = document.querySelector('.user');
+const sidebar = document.querySelector('.sidebar');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
+const xBtn = document.querySelector('.sidebar-header i');
+user.addEventListener('click', () => 
+{
+   sidebar.classList.add('sidebar-display');
+   sidebarWrapper.classList.add('sidebar-wrapper-display');
+});
+
+xBtn.addEventListener('click', () => {
+   sidebar.classList.remove('sidebar-display');
+   sidebarWrapper.classList.remove('sidebar-wrapper-display');
+});
